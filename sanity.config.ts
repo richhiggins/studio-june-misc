cc5f08349e48526ee87593746d9c4cf6dad847e8
Rich Higgins <hello@iamrichhiggins.com>
2025-06-12 14:01:06 +0100

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Studio Latest June',
+
+  projectId: '509lu81n',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
