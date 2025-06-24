1f9d3153b0d86d1af6c247ea415bda82e90579e1
Rich Higgins <hello@iamrichhiggins.com>
2025-06-24 12:26:10 +0100

    moved puma logo

--- myLogo.tsx
+++ myLogo.tsx
@@ -1,5 +1,5 @@
 export const MyLogo = (
   <>
-    <img src="/favicon-152x152.png" alt="My Logo" width={25} height={25} />
+    <img src="/assets/puma-logo.png" alt="My Logo" width={25} height={25} />
   </>
 )
